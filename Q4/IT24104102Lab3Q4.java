c3f7dfcba4393efb374fff15a3d23b752fdab4c7
IT24104102 <it24104102@my.sliit.lk>
2024-11-28 20:02:05 +0530

    Add IT24104102Lab3Q4 to Q4

---
+++ Q4/IT24104102Lab3Q4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class IT24104102Lab3Q4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a five-digit number: ");
+        int number = scanner.nextInt();
+
+        if (number >= 10000 && number <= 99999) {
+            String numberString = Integer.toString(number);
+            for (int i = 0; i < numberString.length(); i++) {
+                System.out.print(numberString.charAt(i) + " ");
+            }
+        }
+        
+    }
+}
